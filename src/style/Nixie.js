946e716a6fb58c9edbee1ed8efef639d3dc98bcd
Dom <dominwong4@gmail.com>
2020-11-28 14:58:59 +0800

    add reflection

--- src/style/Nixie.js
+++ src/style/Nixie.js
@@ -17,15 +17,26 @@ export const StyledNumebrContainer = styled.div`
 export const StyledNumber = styled.div.attrs({})`
   position: absolute;
   font-family: "TT Chocolates ExtraLight";
-  font-size: 10rem;
-  font-size: 15vw;
+  font-size: 16vw;
+  @media screen and (min-width: 900px) {
+    font-size: 10rem;
+  }
   color: #61616130;
   transform: scale(1, 1.3);
   z-index: -1;
   &.active {
     color: #ffb633;
-    text-shadow: 0 0 20px #ff7500;
+    text-shadow: 0 0 20px #ff7500, 0 0 40px #ff7500, 0 0 60px #ff7500,
+      0 0 80px #ff7500, 0 0 100px #ff7500;
     z-index: 2;
+    -webkit-box-reflect: below -2.5rem -webkit-gradient(
+        linear,
+        left top,
+        left bottom,
+        from(transparent),
+        color-stop(55%, transparent),
+        to(rgba(255, 255, 255, 0.2))
+      );
   }
   &.active:before {
     content: "";
@@ -37,7 +48,7 @@ export const StyledNumber = styled.div.attrs({})`
     background: #ff7f12;
     z-index: 0;
     opacity: 0.2;
-    filter: blur(30px);
+    filter: blur(35px);
   }
   &.active:after {
     content: ${(props) => props.number};
@@ -47,6 +58,6 @@ export const StyledNumber = styled.div.attrs({})`
     padding: 0 20px;
     z-index: 1;
     color: #ff005b;
-    filter: blur(15px);
+    filter: blur(25px);
   }
 `;
